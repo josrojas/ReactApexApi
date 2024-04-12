60981cd5cfc7267f3e1cdfd27770c0a0a42d406c
Jose <jos.rojas97@hotmail.com>
2024-04-12 14:50:57 -0500

    Fix css

--- src/components/events/UpgradeLegends/UpgradeLegends.jsx
+++ src/components/events/UpgradeLegends/UpgradeLegends.jsx
@@ -11,9 +11,8 @@ const UpgradesStatus = () => {
             <h1>Legends Upgrades</h1>
 
             <div className={styles.upgradeContainer}>
-                <h2>Assault</h2>
-
                 <div className={styles.pbContainer}>
+                    <h2>Assault</h2>
                     <h3>Ashe</h3>
                     <img src="https://media.contentapi.ea.com/content/dam/apex-legends/common/breakout-patch-notes/legend-assets/assault/ash.jpg" width={300} height={500}></img>
                     <h4>Level 2 Upgrade Options</h4>
