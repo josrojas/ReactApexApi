46ef6ddca12129a6df87b83148120c6bfabc2e46
Jose <jos.rojas97@hotmail.com>
2025-01-23 15:56:50 -0500

    Vercel SpeedInsights

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import Routes from './routes';
 import './App.css'
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 function App() {
     return <Routes />
 }
-
+<SpeedInsights />
 export default App
